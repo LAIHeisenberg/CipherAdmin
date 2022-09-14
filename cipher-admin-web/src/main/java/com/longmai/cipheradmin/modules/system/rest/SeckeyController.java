e2c4bb0374972ba404c5682cbb843a77a24d1e8c
huangsi <huangsi@longmai.com.cn>
2022-09-14 12:19:39 +0800

    user

--- cipher-admin-web/src/main/java/com/longmai/cipheradmin/modules/system/rest/SeckeyController.java
+++ cipher-admin-web/src/main/java/com/longmai/cipheradmin/modules/system/rest/SeckeyController.java
@@ -1,11 +1,14 @@
 package com.longmai.cipheradmin.modules.system.rest;
 
 import com.longmai.cipheradmin.modules.bs.service.Kmip;
+import com.longmai.cipheradmin.modules.bs.service.KmsCryptographicObjectService;
+import com.longmai.cipheradmin.modules.bs.service.dto.KmsCryptographicObjectQueryCriteria;
 import com.longmai.cipheradmin.modules.system.param.SecKeyCreateParam;
 import com.longmai.cipheradmin.utils.PageUtil;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,11 +23,13 @@ public class SeckeyController {
 
     @Autowired
     Kmip kmip;
+    @Autowired
+    KmsCryptographicObjectService kmsCryptographicObjectService;
 
     @GetMapping("/list")
     @PreAuthorize("@el.check('seckey:manage')")
-    public ResponseEntity<Object> listSeckey(){
-        return new ResponseEntity<>(PageUtil.toPage(null,0),HttpStatus.OK);
+    public ResponseEntity<Object> listSeckey(KmsCryptographicObjectQueryCriteria criteria, Pageable pageable){
+        return new ResponseEntity<>(kmsCryptographicObjectService.queryAll(criteria,pageable),HttpStatus.OK);
     }
 
     @PostMapping("/create")
